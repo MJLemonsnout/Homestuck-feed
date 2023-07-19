59b23efc2d918411b33aefb2a3cc7ad4ddef782a
MJLemonsnout <139588575+MJLemonsnout@users.noreply.github.com>
2023-07-19 15:31:59 -0400

    Update subscription.ts
    homestuck-ify per https://github.com/bluesky-social/feed-generator/compare/main...EddyLuten:ttrpg-feed-generator:main

--- src/subscription.ts
+++ src/subscription.ts
@@ -4,26 +4,140 @@ import {
 } from './lexicon/types/com/atproto/sync/subscribeRepos'
 import { FirehoseSubscriptionBase, getOpsByType } from './util/subscription'
 
+const matchText: string[] = [
+  // very common, match these first
+  '#homestuck',
+  '#hom3stuck',
+  '#hiveswap',
+  '#fantroll',
+  '#trollsona',
+  '#mspa',
+  '#sburb',
+  '#sahcon',
+  '#vasterror'
+
+  // broader terms, yet still common
+  'homestuck',
+  'hiveswap',
+  'mspa',
+  'ms paint adventures',
+  'sburb',
+  'tabletop roleplaying game',
+  'tabletop roleplaying games',
+  'tabletop rpgs',
+
+  // publishers
+  'andrew hussie',
+  'hussie',
+  'mspfa',
+
+  // ships - will add more
+  'davekat',
+  '#davekat',
+  'rosemary',
+  '#rosemary',
+  'vrisrezi',
+  '#vrisrezi',
+  'les8ifins',
+  '#les8ifins',
+  'meowrails',
+  '#meowrails',
+  'gamrezi',
+  '#gamrezi',
+  'johnvris',
+  '#johnvris',
+  'daverezi',
+  '#daverezi',
+  'arasol',
+  '#arasol',
+  'pormara',
+  '#pormara',
+  'dirkjake',
+  '#dirkjake',
+  'johnkat',
+  '#johnkat',
+  
+  // characters - just egbert for now
+  'john egbert',
+  'june egbert',
+  'egbert',
+  '#johnegbert',
+  '#juneegbert',
+  'ghostytrickster',
+  'ectobiologist',
+
+  'lemonsnout',
+
+  // terms
+  'alchemiter',
+  'alternia',
+  'auspistice',
+  'auspisticism',
+  'boondollars', 
+  'broadwaystuck',
+  'carapacian',
+  'chumhandle',
+  'classpect',
+  'cruxite',
+  'ectobiology',
+  'fantroll',
+  'gristtorrent',
+  'hemospectrum',
+  'hemotyping',
+  'incipisphere',
+  'kismesis',
+  'kismesissitude',
+  'legislacerator',
+  'matesprit',
+  'matespritship',
+  'moirail',
+  'moirallegiance',
+  'paradoxspace',
+  'paradox space',
+  'pesterchum',
+  'sburb',
+  'scalemate',
+  'skaia',
+  'skaianet', 
+  'subjugglator',
+  'subjugglation',
+  'sylladex',
+  'trollsona',
+
+const matchPatterns: RegExp[] = [
+  /(^|[\s\W])h[o|0]m[e|3]stuck($|[\W\s])/im,
+]
+
+// Include high profile Homestuck fans here to always include their posts
+const matchUsers: string[] = [
+  //
+]
+
+// Exclude posts from these users
+const bannedUsers: string[] = [
+  //
+]
+
+
 export class FirehoseSubscription extends FirehoseSubscriptionBase {
   async handleEvent(evt: RepoEvent) {
     if (!isCommit(evt)) return
     const ops = await getOpsByType(evt)
 
-    // This logs the text of every post off the firehose.
-    // Just for fun :)
-    // Delete before actually using
-    for (const post of ops.posts.creates) {
-      console.log(post.record.text)
-    }
-
     const postsToDelete = ops.posts.deletes.map((del) => del.uri)
     const postsToCreate = ops.posts.creates
       .filter((create) => {
-        // only alf-related posts
-        return create.record.text.toLowerCase().includes('alf')
+        const txt = create.record.text.toLowerCase()
+        return (
+          (matchText.some((term) => txt.includes(term)) ||
+            matchPatterns.some((pattern) => pattern.test(txt)) ||
+            matchUsers.includes(create.author)) &&
+          !bannedUsers.includes(create.author)
+        )
       })
       .map((create) => {
-        // map alf-related posts to a db row
+        console.log(`Found post by ${create.author}: ${create.record.text}`)
+        
         return {
           uri: create.uri,
           cid: create.cid,
