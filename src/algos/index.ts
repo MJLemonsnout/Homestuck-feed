240ffb5026ca3506755692aa620f283238bf08bd
MJLemonsnout <139588575+MJLemonsnout@users.noreply.github.com>
2023-07-19 15:45:56 -0400

    Update index.ts for homestuck
    homestuck

--- src/algos/index.ts
+++ src/algos/index.ts
@@ -3,12 +3,12 @@ import {
   QueryParams,
   OutputSchema as AlgoOutput,
 } from '../lexicon/types/app/bsky/feed/getFeedSkeleton'
-import * as whatsAlf from './whats-alf'
+import * as homestuck from './homestuck'
 
 type AlgoHandler = (ctx: AppContext, params: QueryParams) => Promise<AlgoOutput>
 
 const algos: Record<string, AlgoHandler> = {
-  [whatsAlf.shortname]: whatsAlf.handler,
+  [homestuck.shortname]: homestuck.handler,
 }
 
 export default algos
